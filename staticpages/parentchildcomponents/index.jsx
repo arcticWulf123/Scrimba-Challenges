07e9a3a0c8d4fd550d1341e9c5480cb25f44c26a
jade roland eduard <jaderolandeduard.pinon@lorma.edu>
2026-08-08 22:16:35 +0800

    parentchildcomponent challenge completed

---
+++ staticpages/parentchildcomponents/index.jsx
@@ -0,0 +1,68 @@
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+/** Mini Challenge:
+ *
+ * Move the `header` element from the Page component into
+ * its own component called "Header"
+ *
+ * Then render an instance of the Header component inside
+ * the Page component where the `header` used to be.
+ */
+
+/** Challenge:
+ * Move the `main` element into its own component called "MainContent"
+ * and render that component inside the Page component.
+ *
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+ */
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Reason I am excited to learn React</h1>
+      <ol>
+        <li>
+          React is a popular library, so I will be able to fit in with all the
+          coolest devs out there! 😎
+        </li>
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>
+      </ol>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2024 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <>
+      <Page />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="react-logo.png" width="40px" alt="React logo" />
+    </header>
+  );
+}
+
+root.render(
+  <>
+    <Page />
+  </>,
+);
